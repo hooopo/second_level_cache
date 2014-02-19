0e059dbaa7367ae167da8b3f77d05281de844d9a
Hooopo <hoooopo@gmail.com>
2014-02-19 21:30:24 +0800

    fix test

--- lib/second_level_cache/active_record/preloader.rb
+++ lib/second_level_cache/active_record/preloader.rb
@@ -17,12 +17,11 @@ module SecondLevelCache
 
             map_cache_keys = ids.map{|id| klass.second_level_cache_key(id)}
             records_from_cache = ::SecondLevelCache.cache_store.read_multi(*map_cache_keys)
-
             # NOTICE 
             # Rails.cache.read_multi return hash that has keys only hitted.
             # eg. Rails.cache.read_multi(1,2,3) => {2 => hit_value, 3 => hit_value} 
-            hitted_ids = records_from_cache.map{|key, _| key.split("/")[2]}
-            missed_ids = ids - hitted_ids
+            hitted_ids = records_from_cache.map{|key, _| key.split("/")[2].to_i}
+            missed_ids = ids.map{|x| x.to_i} - hitted_ids
 
             ::SecondLevelCache::Config.logger.info "missed ids -> #{missed_ids.inspect} | hitted ids -> #{hitted_ids.inspect}"
 
