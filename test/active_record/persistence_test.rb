eb7b9cec3d181ed1b2dc0d8c5b0e326008087a63
stopdropandrew <stopdropandrew@gmail.com>
2014-09-12 10:26:57 -0700

    Reload should expire first

--- test/active_record/persistence_test.rb
+++ test/active_record/persistence_test.rb
@@ -8,6 +8,11 @@ class ActiveRecord::PersistenceTest < Minitest::Test
   end
 
   def test_should_reload_object
+    User.where(id: @user.id).update_all(email: 'different@csdn.com')
+    assert_equal 'different@csdn.com', @user.reload.email
+  end
+
+  def test_should_reload_object_associations
     User.increment_counter :books_count, @user.id
     assert_equal 0, @user.books_count
     assert_equal 1, @user.reload.books_count
