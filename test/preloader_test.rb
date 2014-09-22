497e51d5cb7b58abfad69743994a4c9dc3d928f9
stopdropandrew <stopdropandrew@gmail.com>
2014-09-22 14:28:53 -0700

    Make preloaded tests more explicit in their assertions.
    Drop test logging down to ERROR.

--- test/preloader_test.rb
+++ test/preloader_test.rb
@@ -2,30 +2,38 @@
 require 'test_helper'
 
 class PreloaderTest < ActiveSupport::TestCase
-  def setup
-    @topic1 = Topic.create :title => "title1", :body => "body1"
-    @topic2 = Topic.create :title => "title2", :body => "body2"
-    @topic3 = Topic.create :title => "title3", :body => "body3"
-    @post1 = @topic1.posts.create :body => "post1"
-    @post2 = @topic2.posts.create :body => "post2"
-    @post3 = @topic3.posts.create :body => "post3"
-  end
-
-  def test_preload_work_properly
-    results = Post.includes(:topic).order("id ASC").to_a
-    assert_equal results.size, 3
-    assert_equal results.first.topic, @topic1
-  end
-
-  def test_when_read_multi_missed_from_cache_AR_will_fetch_missed_records_from_db
-    @topic1.expire_second_level_cache
+  def test_belongs_to_preload_caches_includes
+    topics = [
+      Topic.create(title: 'title1', body: 'body1'),
+      Topic.create(title: 'title2', body: 'body2'),
+      Topic.create(title: 'title3', body: 'body3')
+    ]
+    topics.each { |topic| topic.posts.create(body: "post#{topic.id}") }
 
     results = nil
-    assert_sql(/IN\s+\(#{@topic1.id}\)/m) do
-      results = Post.includes(:topic).order("id ASC").to_a
+    assert_queries(1) do
+      results = Post.includes(:topic).order('id ASC').to_a
+    end
+    assert_equal topics, results.map(&:topic)
+  end
+
+  def test_belongs_to_when_read_multi_missed_from_cache_AR_will_fetch_missed_records_from_db
+    topics = [
+      Topic.create(title: 'title1', body: 'body1'),
+      Topic.create(title: 'title2', body: 'body2'),
+      Topic.create(title: 'title3', body: 'body3')
+    ]
+    topics.each { |topic| topic.posts.create(body: "post#{topic.id}") }
+    expired_topic = topics.first
+    expired_topic.expire_second_level_cache
+
+    results = nil
+    assert_queries(2) do
+      assert_sql(/IN\s+\(#{expired_topic.id}\)/m) do
+        results = Post.includes(:topic).order('id ASC').to_a
+      end
     end
 
-    assert_equal 3, results.size
-    assert_equal @topic1, results.first.topic
+    assert_equal topics, results.map(&:topic)
   end
 end
