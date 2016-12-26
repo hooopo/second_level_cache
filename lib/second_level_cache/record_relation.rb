640641fd62a5f091b406b34c3bb40c635c10e97b
Jason Lee <huacnlee@gmail.com>
2016-12-26 22:17:52 +0800

    SecondLevelCache::RecordRelation add more comment for someday to remove.

--- lib/second_level_cache/record_relation.rb
+++ lib/second_level_cache/record_relation.rb
@@ -1,13 +1,15 @@
 module SecondLevelCache
   class RecordRelation < Array
-    # A fake Array for fix Rails 5.0.1 records_for method changed bug
-    # in Rails 5.0.0 called:
-    #   records_for()
+    # A fake Array for fix ActiveRecord 5.0.1 records_for method changed bug
+    #
+    # in ActiveRecord 5.0.0 called:
+    #   records_for(slice)
     #
     # but 5.0.1 called:
-    #   records_for().load(&block)
+    #   https://github.com/rails/rails/blob/master/activerecord/lib/active_record/associations/preloader/association.rb#L118
+    #   records_for(slice).load(&block)
     #
-    # https://github.com/rails/rails/pull/26340/
+    # https://github.com/rails/rails/pull/26340
     def load(&block)
       return self
     end
