b9798fe1038eb5f8f98aa65d5f9ce14776cdbc07
Jason Lee <huacnlee@gmail.com>
2017-01-04 11:27:24 +0800

    Fix #45 for model `store` serialize support.
    http://api.rubyonrails.org/classes/ActiveRecord/Store/ClassMethods.html\#method-i-store

--- test/record_marshal_test.rb
+++ test/record_marshal_test.rb
@@ -4,7 +4,7 @@ class RecordMarshalTest < ActiveSupport::TestCase
   def setup
     if ::ActiveRecord::VERSION::STRING >= '4.1.0'
       @json_options = { 'name' => 'Test', 'age' => 18, 'hash' => { 'name' => 'dup' } }
-      @extras = { tagline: "Hello world", gender: 1 }
+      @extras = { 'tagline' => 'Hello world', 'gender' => 1 }
       @user = User.create name: 'csdn',
                           email: 'test@csdn.com',
                           options: [1, 2],
@@ -34,6 +34,8 @@ class RecordMarshalTest < ActiveSupport::TestCase
     assert_equal User.read_second_level_cache(@user.id).changed?, false
     assert_equal [1, 2], User.read_second_level_cache(@user.id).options
     assert_equal @extras, User.read_second_level_cache(@user.id).extras
+    assert_equal 'Hello world', User.read_second_level_cache(@user.id).tagline
+    assert_equal 1, User.read_second_level_cache(@user.id).gender
     if ::ActiveRecord::VERSION::STRING >= '4.1.0'
       result = User.read_second_level_cache(@user.id)
       assert_equal @json_options['name'], result.json_options['name']
