4191e846064ac63308ff513cd9585bf11fa7a8bc
Jason Lee <huacnlee@gmail.com>
2019-09-09 17:14:03 +0800

    Revert "Fix cache expire issue for SoftDelete cases"

--- lib/second_level_cache/active_record/base.rb
+++ lib/second_level_cache/active_record/base.rb
@@ -6,7 +6,7 @@ module SecondLevelCache
       def self.prepended(base)
         base.after_commit :update_second_level_cache, on: :update
         base.after_commit :write_second_level_cache, on: :create
-        if defined?(::Paranoia) || column_names.include?("deleted_at")
+        if defined?(::Paranoia)
           base.after_destroy :expire_second_level_cache
         else
           base.after_commit :expire_second_level_cache, on: :destroy
