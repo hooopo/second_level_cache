57ba92a9c4bd12249a88af944da3c9d82e26136d
Jason Lee <huacnlee@gmail.com>
2019-12-29 20:29:02 +0800

    Fix methods argument as keyword warning in Ruby 2.7

--- lib/second_level_cache/active_record/persistence.rb
+++ lib/second_level_cache/active_record/persistence.rb
@@ -4,17 +4,21 @@ module SecondLevelCache
   module ActiveRecord
     module Persistence
       # update_column will call update_columns
+      # https://github.com/rails/rails/blob/5-0-stable/activerecord/lib/active_record/persistence.rb#L315
       def update_columns(attributes)
         super(attributes).tap { update_second_level_cache }
       end
 
+      # https://github.com/rails/rails/blob/5-0-stable/activerecord/lib/active_record/persistence.rb#L441
       def reload(options = nil)
         expire_second_level_cache
         super(options)
       end
 
-      def touch(*names)
-        super(*names).tap { update_second_level_cache }
+      # https://github.com/rails/rails/blob/5-0-stable/activerecord/lib/active_record/persistence.rb#L490
+      def touch(*names, **opts)
+        # super: touch(*names, time: nil)
+        super(*names, **opts).tap { update_second_level_cache }
       end
     end
   end
