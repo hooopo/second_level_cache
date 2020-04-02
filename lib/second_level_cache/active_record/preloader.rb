f1b80d95b763b8513e024a4000251888ed5cc7b7
OuYangJinTing <2729877005qq@gmail.com>
2020-04-03 00:45:13 +0800

    Fix: adapt rails 5.2

--- lib/second_level_cache/active_record/preloader.rb
+++ lib/second_level_cache/active_record/preloader.rb
@@ -16,13 +16,18 @@ module SecondLevelCache
             return super(ids, &block)
           end
           records_from_cache = ::SecondLevelCache.cache_store.read_multi(*map_cache_keys)
-          record_marshals = RecordMarshal.load_multi(records_from_cache.values) do |record|
-            # This block is copy from:
-            # https://github.com/rails/rails/blob/6-0-stable/activerecord/lib/active_record/associations/preloader/association.rb#L101
-            owner = owners_by_key[convert_key(record[association_key_name])].first
-            association = owner.association(reflection.name)
-            association.set_inverse_instance(record)
-          end
+
+          record_marshals = if ::ActiveRecord.version >= ::Gem::Version.new("6")
+                              RecordMarshal.load_multi(records_from_cache.values) do |record|
+                                # This block is copy from:
+                                # https://github.com/rails/rails/blob/6-0-stable/activerecord/lib/active_record/associations/preloader/association.rb#L101
+                                owner = owners_by_key[convert_key(record[association_key_name])].first
+                                association = owner.association(reflection.name)
+                                association.set_inverse_instance(record)
+                              end
+                            else
+                              RecordMarshal.load_multi(records_from_cache.values, &block)
+                            end
 
           # NOTICE
           # Rails.cache.read_multi return hash that has keys only hitted.
