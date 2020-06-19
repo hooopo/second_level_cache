fde945011026ef2f79858c2a40c8b02aff3cffba
OuYangJinTing <2729877005qq@gmail.com>
2020-06-19 21:33:24 +0800

    use short SHA1 digest instead of MD5 digest

--- lib/second_level_cache/mixin.rb
+++ lib/second_level_cache/mixin.rb
@@ -22,30 +22,19 @@ module SecondLevelCache
       end
 
       def second_level_cache_enabled?
-        if defined? @second_level_cache_enabled
-          @second_level_cache_enabled == true && SecondLevelCache.cache_enabled?
-        else
-          false
-        end
+        @second_level_cache_enabled == true && SecondLevelCache.cache_enabled?
       end
 
       def without_second_level_cache(&blk)
         SecondLevelCache.without_second_level_cache(&blk) if blk
       end
 
-      # Get MD5 digest of this Model schema
-      # http://api.rubyonrails.org/classes/ActiveRecord/Core/ClassMethods.html#method-i-inspect
       def cache_version
-        return @cache_version if defined? @cache_version
-        # This line is copy from:
-        # https://github.com/rails/rails/blob/f9a5f48/activerecord/lib/active_record/core.rb#L236
-        attr_list = attribute_types.map { |name, type| "#{name}: #{type.type}" } * ", "
-        model_schema_digest = Digest::MD5.hexdigest(attr_list)
-        @cache_version = "#{second_level_cache_options[:version]}/#{model_schema_digest}"
+        @cache_version ||= "#{second_level_cache_options[:version]}/#{Digest::SHA1.hexdigest(base_class.inspect).first(7)}"
       end
 
       def second_level_cache_key(id)
-        "#{cache_key_prefix}/#{table_name.downcase}/#{id}/#{cache_version}"
+        "#{cache_key_prefix}/#{table_name}/#{id}/#{cache_version}"
       end
 
       def read_second_level_cache(id, &block)
