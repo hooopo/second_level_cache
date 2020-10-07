d45efc95f7b7d32567b81c408a39756ec3f3d009
moioo <moioo@qq.com>
2020-10-07 16:40:26 +0800

    has_one through目标类配置可能会导致错误

--- test/model/post.rb
+++ test/model/post.rb
@@ -6,7 +6,17 @@ ActiveRecord::Base.connection.create_table(:posts, force: true) do |t|
   t.integer :topic_id
 end
 
+ActiveRecord::Base.connection.create_table(:hotspots, force: true) do |t|
+  t.integer :post_id
+  t.string :summary
+end
+
 class Post < ApplicationRecord
   second_level_cache
   belongs_to :topic, touch: true
+end
+
+class Hotspot < ApplicationRecord
+	belongs_to :post, required: false
+	has_one :topic, through: :post
 end
